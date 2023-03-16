d21383d40785ea6a3d97cbd64727873041271e0d
Irina <ga-irina@yandex.ru>
2023-03-16 16:28:08 +0300

    Добавила поля класса Human

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ class Human:
     city: str
     home_address: str
 
+    def __init__(self, title_book: str, year_release: int, publishing_house: str,
+                 genre: str, author: str, price: float):
+        """Конструктор класса Book"""
+        self.title_book = title_book
+        self.year_release = year_release
+        self.publishing_house = publishing_house
+        self.genre = genre
+        self.author = author
+        self.price = price
+
+    def __str__(self):
+        return f"Название книги: {self.title_book} \n" \
+               f"Год выпуска: {self.year_release} \n" \
+               f"Издатель: {self.publishing_house} \n" \
+               f"Жанр: {self.genre} \n" \
+               f"Автор: {self.author} \n" \
+               f"Цена: {self.price} \n"
 
 
 
