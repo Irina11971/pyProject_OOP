7f7f527b22b2822f18836a5503a326f0fe63adaa
Irina <ga-irina@yandex.ru>
2023-03-18 22:31:20 +0300

    Создала класс Stadium

--- 2_Классы_11.04.23.py
+++ 2_Классы_11.04.23.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 # Задание 1.
 # Реализуйте класс «Автомобиль». Необходимо хранить в полях класса:
 # название модели, год выпуска, производителя, объем двигателя, цвет машины,
@@ -5,7 +7,8 @@
 
 class Car:
 
-    def __init__(self, model: str, year_release: int, manufacturer: str, engine_capacity: float, colour: str, price: int):
+    def __init__(self, model: str, year_release: int, manufacturer: str, engine_capacity: float, colour: str,
+                 price: int = None):
         """Конструктор класса Car"""
         self.model = model
         self.year_release = year_release
@@ -23,17 +26,33 @@ class Car:
                f"Цена: {self.price} \n"
 
 
-
-
 # Задание 2.
 # Реализуйте класс «Стадион». Необходимо хранить в полях класса:
 # название стадиона, дату открытия, страну, город, вместимость. Реализуйте
 # конструктор по умолчанию и метод для вывода данных.
 
+class Stadium:
+
+    def __init__(self,name: str, opening_date: Dict[str, str], country: str, city: str, number_seats: int = None):
+        """Конструктор класса Stadium"""
+        self.name = name
+        self.opening_date = opening_date
+        self.country = country
+        self.city = city
+        self.number_seats = number_seats
+    def __str__(self):
+        return f"Название стадиона: {self.name} \n" \
+               f"Дата открытия: {list(self.opening_date.values())} \n"\
+               f"Страна: {self.country} \n"\
+               f"Город: {self.city} \n"\
+               f"Вместимость: {self.number_seats} \n"
+
 
 
 
 def execute_application():
+    """
+    # Задание 1
 
     car1 = Car("Volkswagen Caravelle", 2021,"Volkswagen", 2.0, "черный", 4150000)
     car2 = Car("Suzuki SX4", 2011,"Volkswagen", 1.6, "серебристый", 915000)
@@ -43,6 +62,24 @@ def execute_application():
     cars.append(car2)
     for car in cars:
         print(car)
+
+    """
+    # Задание 2
+
+    stadium1 = Stadium("Стадион 1 мая", {"число": 1, "месяц": "мая", "год": 1989}, "Северная Корея",
+                       "Пхеньян", 114000)
+    stadium2 = Stadium("Лужники", {"число": 31, "месяц": "июля", "год": 195}, "Россия",
+                       "Москва", 76880)
+    stadium3 = Stadium("Камп Ноу", {"число": 24, "месяц": "сентября", "год": 1957}, "Испания",
+                       "Барселона", 99354)
+
+    stadiums = []
+    stadiums.append(stadium1)
+    stadiums.append(stadium2)
+    stadiums.append(stadium3)
+
+    for stadium in stadiums:
+        print(stadium)
 
 
 if __name__ == "__main__":
