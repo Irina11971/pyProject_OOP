f2e43bec5a88f17fea7e6ebdfa260b328d24f2d2
Irina <ga-irina@yandex.ru>
2023-03-20 21:27:46 +0300

    Создала класс Book

--- 3_13.04.23_Методы классов.py
+++ 3_13.04.23_Методы классов.py
@@ -44,17 +44,6 @@ class Human:
         # self.__date_birth["год"] = date_birth["год"]
 
 
-
-
-# Задание 2.
-# Реализуйте класс «Книга». Необходимо хранить в полях класса:
-# название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
-# конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
-# отдельным полям класса через методы класса (геттеры и сеттеры).
-
-
-
-
 def execute_application():
 
 
@@ -80,6 +69,54 @@ def execute_application():
     # humans.append(human2)
     # for human in humans:
     #     print(human)
+
+
+if __name__ == "__main__":
+    execute_application()
+
+
+# Задание 2.
+# Реализуйте класс «Книга». Необходимо хранить в полях класса:
+# название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
+# конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
+# отдельным полям класса через методы класса (геттеры и сеттеры).
+
+
+class Book:
+    title_book: str
+    year_release: int
+    publishing_house: str
+    genre: str
+    author: str
+    price: float
+
+    def __init__(self, title_book: str, year_release: int, publishing_house: str,
+                 genre: str, author: str, price: float):
+        """Конструктор класса Book"""
+        self.title_book = title_book
+        self.year_release = year_release
+        self.publishing_house = publishing_house
+        self.genre = genre
+        self.author = author
+        self.price = price
+
+    def __str__(self):
+        return f"Название книги: {self.title_book} \n" \
+               f"Год выпуска: {self.year_release} \n" \
+               f"Издатель: {self.publishing_house} \n" \
+               f"Жанр: {self.genre} \n" \
+               f"Автор: {self.author} \n" \
+               f"Цена: {self.price} \n"
+
+
+def execute_application():
+    book1 = Book("Изучаем программирование на Python", 2022, "ООО Издательство 'Эксмо'", "Учебник", "Бэрри Пол", 2564.25)
+    book2 = Book("Грешник", 2023, "ООО Издательство 'Эксмо'", "Романтическая проза", "Эмма Скотт", 581.65)
+    library = []
+    library.append(book1)
+    library.append(book2)
+    for book in library:
+        print(book)
 
 
 if __name__ == "__main__":
