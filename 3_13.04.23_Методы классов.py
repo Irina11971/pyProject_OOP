9f45c407d15dde637c53a52c3362018c38d7a13a
Irina <Irina739791739791@gmail.com>
2023-03-21 22:45:29 +0300

    Реализовала класс Book

--- 3_13.04.23_Методы классов.py
+++ 3_13.04.23_Методы классов.py
@@ -10,38 +10,35 @@ class Human:
 
     def __init__(self, name: str, date_birth: Dict[str, str], telephone: str,
                  country: str, city: str, home_address: Dict[str, str]):
-        """Конструктор класса Human"""
+        #Конструктор класса Human
         self.__name = name
-        self.__date_birth = date_birth
+        self.__date_birth = date_birth.copy()
         self.telephone = telephone
         self.country = country
-        self.city = city
-        self.home_address = home_address
+        self.__city = city
+        self.home_address = home_address.copy()
 
     def __str__(self):
         return f"ФИО: {self.__name} \n" \
                f"Дата рождения: {list(self.__date_birth.values())} \n" \
                f"Телефон: {self.telephone} \n" \
                f"Страна: {self.country} \n" \
-               f"Город: {self.city} \n" \
+               f"Город: {self.__city} \n" \
                f"Домашний адрес: {list(self.home_address.values())} \n"
 
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
-
-    def set_name(self, name: str):
+    @name.setter
+    def name(self, name: str):
         self.__name = name
 
     @property
     def date_birth(self):
-        return self.__date_birth
-
+        return list(self.__date_birth.values())
     @date_birth.setter
     def date_birth(self, date_birth: Dict[str, str]):
         self.__date_birth = date_birth.copy()
-        # self.__date_birth["число"] = date_birth["число"]
-        # self.__date_birth["месяц"] = date_birth["месяц"]
-        # self.__date_birth["год"] = date_birth["год"]
 
 
 def execute_application():
@@ -52,23 +49,22 @@ def execute_application():
     human2 = Human("Краснов С.Ю.", {"число": "3", "месяц": "12", "год": "1998"}, "+7 915-634-10-85", "Россия", "Ярославль",
                    {"страна": "Россия", "населенный пункт": "Ярославль", "улица": "Нахимсона", "дом": 15, "квартира": 8})
 
-    # human1.set_name("Иванов Я.Д.")
-    # print(human1.get_name())
-
-    human1.name = "Чернов Я.Д."         # не работает
+    human1.name = "Чернов Я.Д."
     print(human1.name)
-    print()
-    print(human1)
 
-    human1.date_birth = ({"число": "1", "месяц": "1", "год": "1985"})          # не работает
+    human1.date_birth = ({"число": "1", "месяц": "1", "год": "1985"})  # не работает
     print(human1.date_birth)
 
+    human2.name = "Стрельцов О.Н."
+    human2.city = "Париж"
+    print(human2.city)
+    print()
 
-    # humans = []
-    # humans.append(human1)
-    # humans.append(human2)
-    # for human in humans:
-    #     print(human)
+    humans = []
+    humans.append(human1)
+    humans.append(human2)
+    for human in humans:
+        print(human)
 
 
 if __name__ == "__main__":
@@ -80,44 +76,63 @@ if __name__ == "__main__":
 # название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
 # конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
 # отдельным полям класса через методы класса (геттеры и сеттеры).
-
+"""
 
 class Book:
-    title_book: str
-    year_release: int
-    publishing_house: str
-    genre: str
-    author: str
-    price: float
-
     def __init__(self, title_book: str, year_release: int, publishing_house: str,
                  genre: str, author: str, price: float):
-        """Конструктор класса Book"""
-        self.title_book = title_book
-        self.year_release = year_release
+        #Конструктор класса Book
+        self.__title_book = title_book
+        self.__year_release = year_release
         self.publishing_house = publishing_house
         self.genre = genre
         self.author = author
-        self.price = price
+        self.__price = price
 
     def __str__(self):
-        return f"Название книги: {self.title_book} \n" \
-               f"Год выпуска: {self.year_release} \n" \
+        return f"Название книги: {self.__title_book} \n" \
+               f"Год выпуска: {self.__year_release} \n" \
                f"Издатель: {self.publishing_house} \n" \
                f"Жанр: {self.genre} \n" \
                f"Автор: {self.author} \n" \
-               f"Цена: {self.price} \n"
+               f"Цена: {self.__price} \n"
+
+
+    @property
+    def title_book(self):
+        return self.__title_book
+    @title_book.setter
+    def title_book(self, title_book: str):
+        self.__title_book = title_book
+
+    @ property
+    def year_release(self):
+        return self.__year_release
+    @year_release.setter
+    def year_release(self, year_release: int):
+        self.__year_release = year_release
+
+    @property
+    def price(self):
+        return self.__price
+        
+        
+    @price.setter
+    def price(self, price: float):
+        self.__price = price
 
 
 def execute_application():
     book1 = Book("Изучаем программирование на Python", 2022, "ООО Издательство 'Эксмо'", "Учебник", "Бэрри Пол", 2564.25)
-    book2 = Book("Грешник", 2023, "ООО Издательство 'Эксмо'", "Романтическая проза", "Эмма Скотт", 581.65)
-    library = []
-    library.append(book1)
-    library.append(book2)
-    for book in library:
-        print(book)
+ 
+    book1.title_book = "Буратино"
+    book1.year_release = 2023
+    book1.price = 2000
+
+    print(book1)
+
 
 
 if __name__ == "__main__":
     execute_application()
+"""
